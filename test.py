ccb7a6fdaa20f036f5c1d4c534cceb2687c27f02
Ben <ben@ordoro.com>
2010-09-20 18:23:52 -0500

    Setup Fedex testing for certification

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 try:
-    from test_config import UPSConfig, EndiciaConfig, FedexConfig
+    from test_config import UPSConfig, EndiciaConfig
+    from test_config import FedexConfigDebug as FedexConfig
 except:
     print 'HELLO THERE! test_config not found. If you want to run this test, you need to setup test_config.py with your account information.'
     raise
@@ -17,7 +18,7 @@ recipient_intl = Address('Apple', "1 Infinite Loop", 'Cupertino', 'CA', 95014, '
 def _show_file(extension, data):
     with tempfile.NamedTemporaryFile(suffix=extension, delete=False) as temp_file:
         temp_file.write(data)
-        #os.system('open %s' % temp_file.name)
+        os.system('open %s' % temp_file.name)
 
 def TestUPS():
     package = [ None ]
@@ -82,6 +83,166 @@ def TestEndicia():
     # response = request.send()
     # print response
 
+def TestFedexGroundCertification():
+    test_cases = [
+        (
+            Address('TC #4', '20 FedEx Parkway', 'Kansas City', 'MO', 64112, 'US', address2='2nd Floor Suite 2001', phone=5152961616, is_residence=False),
+            fedex.Package(20.0 * 16, 12, 12, 12),
+            fedex.SERVICES[0],
+            fedex.PACKAGES[-1],
+            False, # Email alert
+            False, # Evening
+        ),
+        (
+            Address('TC #20', '456 Roswell Rd', 'Atlanta', 'GA', 30328, 'US', phone=5152961616),
+            fedex.Package(20.0 * 16, 5, 5, 108),
+            fedex.SERVICES[1],
+            fedex.PACKAGES[-1],
+            False, # Email alert
+            False, # Evening
+        ),
+        (
+            Address('TC #21', '987 Main St', 'Boston', 'MA', '02129', 'US', phone=5152961616),
+            fedex.Package(20.0 * 16, 5, 5, 108, require_signature=True),
+            fedex.SERVICES[1],
+            fedex.PACKAGES[-1],
+            False, # Email alert
+            False, # Evening
+        ),
+        (
+            Address('TC #23', '321 Jay St', 'New York', 'NY', '11201', 'US', phone=5152961616),
+            fedex.Package(20.0 * 16, 5, 5, 108, require_signature=True),
+            fedex.SERVICES[1],
+            fedex.PACKAGES[-1],
+            False, # Email alert
+            True,  # Evening
+        ),
+        (
+            Address('TC #24', '456 Roswell Rd', 'Atlanta', 'GA', 30328, 'US', phone=5152961616),
+            fedex.Package(20.0 * 16, 5, 5, 108, require_signature=True),
+            fedex.SERVICES[1],
+            fedex.PACKAGES[-1],
+            False, # Email alert
+            True,  # Evening
+        ),
+    ]
+    
+    f = fedex.Fedex(FedexConfig)
+    
+    for case in test_cases:
+        try:
+            print case
+            recipient, package, service, package_type, email, evening = case
+            packages = [ package ]
+            response = f.label(packages, package_type, service, shipper, recipient, email, evening=evening)
+            
+            for info in response['info']:
+                _show_file(extension='.png', data=info['label'])
+        except fedex.FedexError as e:
+            print e
+
+def TestFedexExpressCertification():
+    test_cases = [
+        (
+            Address('TC #1', '123 Bishop Rd', 'Honolulu', 'HI', 96819, 'US', phone=5152961616),
+            fedex.Package(1.0 * 16, 12, 12, 12),
+            fedex.SERVICES[5],
+            fedex.PACKAGES[0],
+            False, # Email alert
+            False, # Evening
+        ),
+        (
+            Address('TC #4', '789 Davies', 'Englewood', 'CO', 80112, 'US', phone=5152961616),
+            fedex.Package(1.0 * 16, 12, 12, 12),
+            fedex.SERVICES[4],
+            fedex.PACKAGES[0],
+            False, # Email alert
+            False, # Evening
+        ),
+        (
+            Address('TC #7', '6050 Rockwell Ave', 'Anchorage', 'AK', 99501, 'US', phone=5152961616),
+            fedex.Package(5.0 * 16, 12, 12, 12),
+            fedex.SERVICES[3],
+            fedex.PACKAGES[1],
+            False, # Email alert
+            False, # Evening
+        ),
+        (
+            Address('TC #8', '44 Binney St', 'Boston', 'MA', 02115, 'US', phone=5152961616),
+            fedex.Package(1.0 * 16, 12, 12, 12),
+            fedex.SERVICES[6],
+            fedex.PACKAGES[0],
+            False, # Email alert
+            False, # Evening
+        ),
+        (
+            Address('TC #9', '16 Court St', 'New York', 'NY', 10211, 'US', phone=5152961616),
+            fedex.Package(8.0 * 16, 7, 10, 15),
+            fedex.SERVICES[6],
+            fedex.PACKAGES[3],
+            False, # Email alert
+            False, # Evening
+        ),
+        (
+            Address('TC #10', 'SW 129th St', 'Miami', 'FL', 33156, 'US', phone=5152961616),
+            fedex.Package(1.0 * 16, 12, 12, 12),
+            fedex.SERVICES[2],
+            fedex.PACKAGES[0],
+            False, # Email alert
+            False, # Evening
+        ),
+        (
+            Address('TC #11', '36 Charles Lane', 'Baltimore', 'MD', 21201, 'US', phone=5152961616),
+            fedex.Package(150 * 16, 10, 10, 15),
+            fedex.SERVICES[2],
+            fedex.PACKAGES[3],
+            False, # Email alert
+            False, # Evening
+        ),
+    ]
+
+    f = fedex.Fedex(FedexConfig)
+
+    for case in test_cases:
+        try:
+            print case
+            recipient, package, service, package_type, email, evening = case
+            packages = [ package ]
+            response = f.label(packages, package_type, service, shipper, recipient, email, evening=evening)
+
+            for info in response['info']:
+                _show_file(extension='.png', data=info['label'])
+        except fedex.FedexError as e:
+            print e
+
+def TestFedexProd():
+    test_cases = [
+        (
+            Address('TC #4', '20 FedEx Parkway', 'Kansas City', 'MO', 64112, 'US', address2='2nd Floor Suite 2001', phone=5152961616, is_residence=False),
+            fedex.Package(20.0 * 16, 12, 12, 12),
+            fedex.SERVICES[0],
+            fedex.PACKAGES[-1],
+            False, # Email alert
+            False, # Evening
+        ),
+    ]
+    
+    from test_config import FedexConfigProd
+    
+    f = fedex.Fedex(FedexConfigProd, debug=False)
+    
+    for case in test_cases:
+        try:
+            print case
+            recipient, package, service, package_type, email, evening = case
+            packages = [ package ]
+            response = f.label(packages, package_type, service, shipper, recipient, email, evening=evening)
+            
+            for info in response['info']:
+                _show_file(extension='.png', data=info['label'])
+        except fedex.FedexError as e:
+            print e
+
 def TestFedex():
     f = fedex.Fedex(FedexConfig)
     
@@ -101,6 +262,8 @@ def TestFedex():
                     _show_file(extension='.png', data=info['label'])
             except fedex.FedexError as e:
                 print e
+            break
+        break
 
 if __name__ == '__main__':
     # import suds
@@ -110,7 +273,10 @@ if __name__ == '__main__':
     # wsdl_url = 'https://www.envmgr.com/LabelService/EwsLabelService.asmx?WSDL'
     # client = Client(wsdl_url)
     # print client
-    TestUPS()
+    #TestUPS()
     #TestUSPS()
     #TestEndicia()
     #TestFedex()
+    #TestFedexGroundCertification()
+    #TestFedexExpressCertification()
+    TestFedexProd()
