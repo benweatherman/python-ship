5684e212ad4473a393607b89c3448e7bf9b1800f
root <tristan@enflick.com>
2013-07-06 13:53:38 -0400

    Minor fixes and additions to keep current with UPS API.

--- ups.py
+++ ups.py
@@ -33,6 +33,7 @@ SERVICES = [
     ('07', 'UPS Worldwide Express'),
     ('08', 'UPS Worldwide Expedited'),
     ('54', 'UPS Worldwide Express Plus'),
+    ('96', 'UPS Worldwide Express Freight'),
 ]
 
 PACKAGES = [
@@ -92,6 +93,7 @@ class UPS(object):
         country_lookup = {
             'usa': 'US',
             'united states': 'US',
+            'canada': 'CA',
         }
         return country_lookup.get(country.lower(), country)
         
@@ -114,6 +116,7 @@ class UPS(object):
 
     def _create_shipment(self, client, packages, shipper_address, recipient_address, box_shape, namespace='ns3', create_reference_number=True, can_add_delivery_confirmation=True):
         shipment = client.factory.create('{}:ShipmentType'.format(namespace))
+        shipper_country = self._normalized_country_code(shipper_address.country)
 
         for i, p in enumerate(packages):
             package = client.factory.create('{}:PackageType'.format(namespace))
@@ -123,10 +126,13 @@ class UPS(object):
             elif hasattr(package, 'PackagingType'):
                 package.PackagingType.Code = box_shape
             
-            package.Dimensions.UnitOfMeasurement.Code = 'IN'
-            package.Dimensions.Length = p.length
-            package.Dimensions.Width = p.width
-            package.Dimensions.Height = p.height
+            if box_shape == PACKAGES[0][0]:
+            	package.Dimensions.UnitOfMeasurement.Code = 'IN'
+            	if (p.length == 0) or (p.width == 0) or (p.height == 0):
+            		raise UPSError('Dimensions',"Packaging dimensions are required if packaging type is custom")
+            	package.Dimensions.Length = p.length
+            	package.Dimensions.Width = p.width
+            	package.Dimensions.Height = p.height
             
             package.PackageWeight.UnitOfMeasurement.Code = 'LBS'
             package.PackageWeight.Weight = p.weight
@@ -136,7 +142,7 @@ class UPS(object):
              
             
             if p.value:
-                package.PackageServiceOptions.DeclaredValue.CurrencyCode = 'USD'
+                package.PackageServiceOptions.DeclaredValue.CurrencyCode = 'CAD' if (shipper_country=='CA') else 'USD'
                 package.PackageServiceOptions.DeclaredValue.MonetaryValue = p.value
             
             if create_reference_number and p.reference:
@@ -153,9 +159,8 @@ class UPS(object):
         shipfrom_company = shipper_address.company_name[:35]
         shipment.Shipper.Name = shipfrom_company or shipfrom_name
         shipment.Shipper.Address.AddressLine = [ shipper_address.address1, shipper_address.address2 ]
-        shipment.Shipper.Address.City = shipper_address.city
+        shipment.Shipper.Address.City = shipper_address.city[:30]
         shipment.Shipper.Address.PostalCode = shipper_address.zip
-        shipper_country = self._normalized_country_code(shipper_address.country)
         shipment.Shipper.Address.CountryCode = shipper_country
         shipment.Shipper.ShipperNumber = self.credentials['shipper_number']
         
@@ -163,7 +168,7 @@ class UPS(object):
         shipto_company = recipient_address.company_name[:35]
         shipment.ShipTo.Name = shipto_company or shipto_name
         shipment.ShipTo.Address.AddressLine = [ recipient_address.address1, recipient_address.address2 ]
-        shipment.ShipTo.Address.City = recipient_address.city
+        shipment.ShipTo.Address.City = recipient_address.city[:30]
         shipment.ShipTo.Address.PostalCode = recipient_address.zip
         recipient_country = self._normalized_country_code(recipient_address.country)
         shipment.ShipTo.Address.CountryCode = recipient_country
@@ -287,7 +292,7 @@ class UPS(object):
         create_reference_number = recipient_address.country in ( 'US', 'CA', 'PR' ) and shipper_address.country == recipient_address.country
         delivery_confirmation = create_reference_number
         shipment = self._create_shipment(client, packages, shipper_address, recipient_address, box_shape, create_reference_number=create_reference_number, can_add_delivery_confirmation=delivery_confirmation)
-        shipment.ShipmentRatingOptions.NegotiatedRates = ''
+        shipment.ShipmentRatingOptions.NegotiatedRatesIndicator = ''
 
         if not create_reference_number:
             reference_number = client.factory.create('ns3:ReferenceNumberType')
@@ -363,6 +368,7 @@ class UPS(object):
                 product.Unit.Number = p.quantity
                 product.Description = p.description[:35]
                 product.OriginCountryCode = self._normalized_country_code(p.country)
+                product.CommodityCode = p.commoditycode
                 shipment.ShipmentServiceOptions.InternationalForms.Product.append(product)
 
         label = client.factory.create('ns3:LabelSpecificationType')
