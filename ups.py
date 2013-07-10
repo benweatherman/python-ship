1384fdbaf27d0b738e3685f7285ffeb0025a492d
r3dfish <joey@CloudCreativeGroup.com>
2013-07-10 10:58:16 -0700

    update ups.py for python < 2.7 support
    fix this error for python 2.6:
    
    $ ./test.py
    Status: Success tracking: 1ZR052A2D491648028, cost: 127.89
    Status: Success tracking: 1ZR052A2D497605830, cost: 127.89
    UPS Error 250065: Invalid or missing request element
    Traceback (most recent call last):
      File "./test.py", line 338, in <module>
        TestUPS()
      File "./test.py", line 74, in TestUPS
        response = u.label(packages, shipper, r, ups.SERVICES[2][0], ups.PACKAGES[7][0], validate, [ 'test@mytrashmail.com '])
      File "/python-ship/ups.py", line 323, in label
        shipment = self._create_shipment(client, packages, shipper_address, recipient_address, box_shape, create_reference_number=create_reference_number, can_add_delivery_confirmation=delivery_confirmation)
      File "/python-ship/ups.py", line 179, in _create_shipment
        reference_number = client.factory.create('{}:ReferenceNumberType'.format(namespace))
    ValueError: zero length field name in format

--- ups.py
+++ ups.py
@@ -176,7 +176,11 @@ class UPS(object):
             
             if create_reference_number and p.reference:
                 try:
-                    reference_number = client.factory.create('{}:ReferenceNumberType'.format(namespace))
+                    ''' try block is for python > 2.7, except block is for python < 2.7 '''
+                    try:
+                        reference_number = client.factory.create('{}:ReferenceNumberType'.format(namespace))
+                    except ValueError:
+                    	reference_number = client.factory.create('{0}:ReferenceNumberType'.format(namespace))
                     reference_number.Value = p.reference
                     package.ReferenceNumber.append(reference_number)
                 except suds.TypeNotFound as e:
