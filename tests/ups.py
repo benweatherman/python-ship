28e4864c27cda792a5e21dcae7b01bcd796ffabd
Ryan Casey <ryepdx@gmail.com>
2013-12-27 13:18:01 -0800

    Split apart tests.py into three files under the 'tests' directory.

---
+++ tests/ups.py
@@ -0,0 +1,89 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import logging
+
+try:
+    from .config import UPSTestConfig
+except:
+    logging.error("Could not find UPSTestConfig in tests.config module.")
+    raise
+
+import os, tempfile
+from shipping import Package, Address, Product
+import sys
+sys.path.append('../')
+
+import ups
+
+shipper = Address('Adobe', "345 Park Avenue", 'San Jose', 'CA', 95110, 'US', phone='5122901212', email='ben@ordoro.com')
+recipient = Address('Apple', "1 Infinite Loop", 'Cupertino', 'CA', 95014, 'US', phone='5122901212', email='ben@ordoro.com')
+bad_recipient = Address('Apple', "1 Park Place.", 'San Diego', 'CA', 95014, 'US', phone='5122901212', email='ben@ordoro.com', address2='#502')
+recipient_intl = Address('Apple', "1 Infinite Loop", 'Cupertino', 'CA', 95014, 'CA', phone='5122901212', email='ben@ordoro.com')
+recipient_intl2 = Address('Bazaarvoice', 'One Lyric Square', 'London', '', 'W6 0NB', 'GB', phone='+44 (0)208 080', email='ben@ordoro.com')
+recipient_intl4 = Address('Some Recipient', '24 Tennant st', 'EDINBURGH', 'Midlothian', 'EH6 5ND', 'GB', phone='+44 (0)208 080', email='ben@ordoro.com')
+
+def _show_file(extension, data):
+    with tempfile.NamedTemporaryFile(suffix=extension, delete=False) as temp_file:
+        temp_file.write(data)
+        os.system('open %s' % temp_file.name)
+
+class P(object):
+    def __init__(self, **kwargs):
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+
+def TestUPS():
+    packages = [ Package(20.0 * 16, 12, 12, 12, value=1000, require_signature=3, reference='a12302b') ]
+
+    intl_packages = [ Package(2.0 * 16, 12, 12, 12, value=100, require_signature=2, reference='a12302b') ]
+    products = [
+        Product(total=10, value=2, quantity=5, description='It\'s just a bunch of widgets', country = 'CA'),
+        Product(total=10, value=2, quantity=5, description='It\'s just a bunch of widgets', country = 'CA')
+    ]
+    
+    u = ups.UPS(UPSConfig, debug=True)
+
+    for r in ( recipient_intl2, recipient_intl4, ):
+        try:
+            validate = True
+            r.is_residence = True
+            response = u.label(intl_packages, shipper, r, ups.SERVICES[9][0], ups.PACKAGES[5][0], validate, [ 'test@mytrashmail.com '], create_commercial_invoice=True, customs_info=products)
+            status = response['status']
+            print 'Status: %s' % status,
+            for info in response['shipments']:
+                print 'tracking: %s, cost: %s' % (info['tracking_number'], info['cost'])
+                # _show_file(extension='.gif', data=info['label'])
+            # _show_file(extension='.pdf', data=response['international_document']['pdf'])
+        except ups.UPSError as e:
+            print e
+    
+    for r in ( recipient, shipper, bad_recipient, recipient_intl ):
+        try:
+            print u.validate(r)
+        except ups.UPSError as e:
+            print e
+        
+        try:
+            validate = False
+            r.is_residence = True
+            response = u.label(packages, shipper, r, ups.SERVICES[2][0], ups.PACKAGES[7][0], validate, [ 'test@mytrashmail.com '])
+            status = response['status']
+            print 'Status: %s' % status,
+            for info in response['shipments']:
+                print 'tracking: %s, cost: %s' % (info['tracking_number'], info['cost'])
+                # _show_file(extension='.gif', data=info['label'])
+        except ups.UPSError as e:
+            print e
+        
+        return
+    
+        for package_type in ups.PACKAGES:
+            package_code = package_type[0]
+            try:
+                response = u.rate(packages, package_code, shipper, r)
+                print response
+            except ups.UPSError as e:
+                print e
+
+if __name__ == '__main__':
+    TestUPS()
---
+++ tests/usps.py
@@ -0,0 +1,148 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import logging
+
+try:
+    from .config import EndiciaTestConfig
+except:
+    logging.error("Could not find EndiciaTestConfig in tests.config module.")
+    raise
+
+import os, tempfile
+from shipping import Package, Address
+import sys
+sys.path.append('../')
+
+import endicia
+
+shipper = Address('Adobe', "345 Park Avenue", 'San Jose', 'CA', 95110, 'US', phone='5122901212', email='ben@ordoro.com')
+recipient = Address('Apple', "1 Infinite Loop", 'Cupertino', 'CA', 95014, 'US', phone='5122901212', email='ben@ordoro.com')
+recipient_intl = Address('Apple', "1 Infinite Loop", 'Cupertino', 'CA', 95014, 'CA', phone='5122901212', email='ben@ordoro.com')
+
+def _show_file(extension, data):
+    with tempfile.NamedTemporaryFile(suffix=extension, delete=False) as temp_file:
+        temp_file.write(data)
+        os.system('open %s' % temp_file.name)
+
+class P(object):
+    def __init__(self, **kwargs):
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+
+def TestEndiciaLabel():
+    package = endicia.Package(endicia.Package.shipment_types[0], 20, endicia.Package.shapes[1], 10, 10, 10)
+    package_intl = endicia.Package(endicia.Package.international_shipment_types[0], 20, endicia.Package.shapes[3], 10, 10, 10)
+    customs = [ endicia.Customs('hello', 1, 2, 100, 'Bermuda'), endicia.Customs('Thingy', 10, 16, 80, 'Bahamas') ]
+    
+    debug = True
+    req0 = endicia.LabelRequest(EndiciaPartnerID, EndiciaAccountID, EndiciaPassphrase, package_intl, shipper, recipient_intl, contents_type='Merchandise', customs_info=customs, debug=debug)
+    req1 = endicia.LabelRequest(EndiciaPartnerID, EndiciaAccountID, EndiciaPassphrase, package, shipper, recipient, debug=debug)
+    req2 = endicia.LabelRequest(EndiciaPartnerID, EndiciaAccountID, EndiciaPassphrase, package, shipper, recipient, stealth=False, debug=debug)
+    req3 = endicia.LabelRequest(EndiciaPartnerID, EndiciaAccountID, EndiciaPassphrase, package, shipper, recipient, insurance='ENDICIA', insurance_amount=1.0, debug=debug)
+    req4 = endicia.LabelRequest(EndiciaPartnerID, EndiciaAccountID, EndiciaPassphrase, package, shipper, recipient, customs_form='Form2976A', customs_info=customs, contents_type='Merchandise', debug=debug)
+
+    for request in [ req0, req1, req2, req3, req4 ]:
+        response = request.send()
+    
+        print response
+        if not isinstance(response, endicia.Error):
+            _show_file(extension='.png', data=response.label)
+    
+    return response
+
+def TestEndicia():
+    debug = True
+    
+    # TestEndiciaLabel()
+
+    # Rate
+    packages = [ Package(20.0 * 16, 12, 12, 12, value=100) ]
+
+    en = endicia.Endicia(EndiciaConfig)
+    for shape in endicia.Package.shapes:
+        try:
+            response = en.rate(packages, shape, shipper, recipient)
+            print response
+        except endicia.EndiciaError as e:
+            print e
+
+    # # Account Status
+    # request = endicia.AccountStatusRequest(EndiciaPartnerID, EndiciaAccountID, EndiciaPassphrase, debug=debug)
+    # response = request.send()
+    # print response
+    
+    # # Recredit
+    # request = endicia.RecreditRequest(EndiciaPartnerID, EndiciaAccountID, EndiciaPassphrase, 10.00, debug=debug)
+    # response = request.send()
+    # print response
+
+    # Change Password
+    # request = endicia.ChangePasswordRequest(EndiciaPartnerID, EndiciaAccountID, EndiciaPassphrase, 'ord7oro', debug=debug)
+    # response = request.send()
+    # print response
+
+    # Refund
+    # request = endicia.RefundRequest(EndiciaPartnerID, EndiciaAccountID, EndiciaPassphrase, tracking_number, debug=debug)
+    # response = request.send()
+    # print response
+
+def TestFedexGroundCertification():
+    test_cases = [
+        (
+            Address('TC #4', '20 FedEx Parkway', 'Kansas City', 'MO', 64112, 'US', address2='2nd Floor Suite 2001', phone=5152961616, is_residence=False),
+            fedex.Package(20.0 * 16, 12, 12, 12),
+            fedex.SERVICES[0],
+            fedex.PACKAGES[-1],
+            False, # Email alert
+            False, # Evening
+        ),
+        (
+            Address('TC #20', '456 Roswell Rd', 'Atlanta', 'GA', 30328, 'US', phone=5152961616),
+            fedex.Package(20.0 * 16, 5, 5, 108),
+            fedex.SERVICES[1],
+            fedex.PACKAGES[-1],
+            False, # Email alert
+            False, # Evening
+        ),
+        (
+            Address('TC #21', '987 Main St', 'Boston', 'MA', '02129', 'US', phone=5152961616),
+            fedex.Package(20.0 * 16, 5, 5, 108, require_signature=True),
+            fedex.SERVICES[1],
+            fedex.PACKAGES[-1],
+            False, # Email alert
+            False, # Evening
+        ),
+        (
+            Address('TC #23', '321 Jay St', 'New York', 'NY', '11201', 'US', phone=5152961616),
+            fedex.Package(20.0 * 16, 5, 5, 108, require_signature=True),
+            fedex.SERVICES[1],
+            fedex.PACKAGES[-1],
+            False, # Email alert
+            True,  # Evening
+        ),
+        (
+            Address('TC #24', '456 Roswell Rd', 'Atlanta', 'GA', 30328, 'US', phone=5152961616),
+            fedex.Package(20.0 * 16, 5, 5, 108, require_signature=True),
+            fedex.SERVICES[1],
+            fedex.PACKAGES[-1],
+            False, # Email alert
+            True,  # Evening
+        ),
+    ]
+    
+    f = fedex.Fedex(FedexConfig)
+    
+    for case in test_cases:
+        try:
+            print case
+            recipient, package, service, package_type, email, evening = case
+            packages = [ package ]
+            response = f.label(packages, package_type, service, shipper, recipient, email, evening=evening)
+            
+            for info in response['info']:
+                _show_file(extension='.png', data=info['label'])
+        except fedex.FedexError as e:
+            print e
+
+if __name__ == '__main__':
+    TestEndicia()
