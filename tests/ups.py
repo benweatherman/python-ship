aad98aca34897978255dfb38d97f95cef68a51cf
Ryan Casey <ryepdx@gmail.com>
2013-12-27 15:02:38 -0800

    Converted UPS test to use the unittest module.

--- tests/ups.py
+++ tests/ups.py
@@ -15,71 +15,60 @@ sys.path.append('../')
 
 import ups
 
-shipper = Address('Adobe', "345 Park Avenue", 'San Jose', 'CA', 95110, 'US', phone='5122901212', email='ben@ordoro.com')
-recipient = Address('Apple', "1 Infinite Loop", 'Cupertino', 'CA', 95014, 'US', phone='5122901212', email='ben@ordoro.com')
-bad_recipient = Address('Apple', "1 Park Place.", 'San Diego', 'CA', 95014, 'US', phone='5122901212', email='ben@ordoro.com', address2='#502')
-recipient_intl = Address('Apple', "1 Infinite Loop", 'Cupertino', 'CA', 95014, 'CA', phone='5122901212', email='ben@ordoro.com')
-recipient_intl2 = Address('Bazaarvoice', 'One Lyric Square', 'London', '', 'W6 0NB', 'GB', phone='+44 (0)208 080', email='ben@ordoro.com')
-recipient_intl4 = Address('Some Recipient', '24 Tennant st', 'EDINBURGH', 'Midlothian', 'EH6 5ND', 'GB', phone='+44 (0)208 080', email='ben@ordoro.com')
+# TODO: Split this up further. Should be one assert per function, and domestic
+# and international tests should probably be two different TestCase classes.
 
+class TestUPS(unittest.TestCase):
+    def setUp(self):
+        self.api = ups.UPS(UPSTestConfig, debug=True)
+        self.shipper = Address('Adobe', "345 Park Avenue", 'San Jose', 'CA', 95110, 'US', phone='5122901212', email='ben@ordoro.com')
+        self.test_email = 'test@mytrashmail.com'
 
-class P(object):
-    def __init__(self, **kwargs):
-        for key, value in kwargs.items():
-            setattr(self, key, value)
-
-def TestUPS():
-    packages = [ Package(20.0 * 16, 12, 12, 12, value=1000, require_signature=3, reference='a12302b') ]
-
-    intl_packages = [ Package(2.0 * 16, 12, 12, 12, value=100, require_signature=2, reference='a12302b') ]
-    products = [
-        Product(total=10, value=2, quantity=5, description='It\'s just a bunch of widgets', country = 'CA'),
-        Product(total=10, value=2, quantity=5, description='It\'s just a bunch of widgets', country = 'CA')
-    ]
-    
-    u = ups.UPS(UPSConfig, debug=True)
-
-    for r in ( recipient_intl2, recipient_intl4, ):
-        try:
+    def TestIntl(self):
+        recipients = [
+            Address('Bazaarvoice', 'One Lyric Square', 'London', '', 'W6 0NB', 'GB', phone='+44 (0)208 080', email='ben@ordoro.com'),
+            Address('Some Recipient', '24 Tennant st', 'EDINBURGH', 'Midlothian', 'EH6 5ND', 'GB', phone='+44 (0)208 080', email='ben@ordoro.com')
+        ]
+        packages = [
+            Package(2.0 * 16, 12, 12, 12, value=100, require_signature=2, reference='a12302b')
+        ]
+        products = [
+            Product(total=10, value=2, quantity=5, description='It\'s just a bunch of widgets', country = 'CA'),
+            Product(total=10, value=2, quantity=5, description='It\'s just a bunch of widgets', country = 'CA')
+        ]
+        
+        for recipient in recipients:
             validate = True
-            r.is_residence = True
-            response = u.label(intl_packages, shipper, r, ups.SERVICES[9][0], ups.PACKAGES[5][0], validate, [ 'test@mytrashmail.com '], create_commercial_invoice=True, customs_info=products)
-            status = response['status']
-            print 'Status: %s' % status,
+            recipient.is_residence = True
+            response = self.api.label(intl_packages, self.shipper, recipient, ups.SERVICES[9][0], ups.PACKAGES[5][0], validate, [ self.test_email ], create_commercial_invoice=True, customs_info=products)
+            self.assertIn("status", response)
+            self.assertIn("shipments", response)
+
             for info in response['shipments']:
-                print 'tracking: %s, cost: %s' % (info['tracking_number'], info['cost'])
-                # _show_file(extension='.gif', data=info['label'])
-            # _show_file(extension='.pdf', data=response['international_document']['pdf'])
-        except ups.UPSError as e:
-            print e
-    
-    for r in ( recipient, shipper, bad_recipient, recipient_intl ):
-        try:
-            print u.validate(r)
-        except ups.UPSError as e:
-            print e
+                self.assertIn("tracking_number", info)
+                self.assertIn("cost", info)
+ 
+    def TestDomestic(self):
+        # Test 'validate' function.
+        recipient = Address('Apple', "1 Infinite Loop", 'Cupertino', 'CA', 95014, 'US', phone='5122901212', email='ben@ordoro.com')
+        self.assertTrue(self.api.validate(recipient)["valid"], msg = "UPS thinks Apple's HQ address isn't valid.")
         
-        try:
-            validate = False
-            r.is_residence = True
-            response = u.label(packages, shipper, r, ups.SERVICES[2][0], ups.PACKAGES[7][0], validate, [ 'test@mytrashmail.com '])
-            status = response['status']
-            print 'Status: %s' % status,
-            for info in response['shipments']:
-                print 'tracking: %s, cost: %s' % (info['tracking_number'], info['cost'])
-                # _show_file(extension='.gif', data=info['label'])
-        except ups.UPSError as e:
-            print e
-        
-        return
+        bad_recipient = Address('Apple', "1 Park Place.", 'San Diego', 'CA', 95014, 'US', phone='5122901212', email='ben@ordoro.com', address2='#502')
+        self.assertFalse(self.api.validate(bad_recipient)["valid"], msg = "UPS thinks our invalid address is valid.")
     
+        # Test 'label' function.
+        validate = False
+        recipient.is_residence = True
+        package = Package(20.0 * 16, 12, 12, 12, value=1000, require_signature=3, reference='a12302b')
+        response = self.api.label([package], self.shipper, recipient, ups.SERVICES[2][0], ups.PACKAGES[7][0], validate, [ self.test_email ])
+        status = response['status']
+        self.assertIn("shipments", response)
+
+        for shipment in response['shipments']:
+            self.assertIn("tracking_number", shipment)
+            self.assertIn("cost", shipment)
+   
+        # Test 'rate' function. 
         for package_type in ups.PACKAGES:
             package_code = package_type[0]
-            try:
-                response = u.rate(packages, package_code, shipper, r)
-                print response
-            except ups.UPSError as e:
-                print e
-
-if __name__ == '__main__':
-    TestUPS()
+            response = self.api.rate(packages, package_code, shipper, r)
