22d3a241b7c3d07aecf67e37797123d627a8ebc1
Ryan Casey <ryepdx@gmail.com>
2014-04-30 08:37:21 -0700

    Add tests config file to repo.

---
+++ tests/config.py
@@ -0,0 +1,43 @@
+# Production config dictionaries.
+UPSConfig = {
+    "username": "<your username>",
+    "password": "<your password>",
+    "access_license": "<your access license>",
+    "shipper_number": "<your shipper number>"
+}
+
+FedexConfig = {
+    "key": "<your API key>",
+    "password": "<your password>",
+    "account_number": "<your account number>",
+    "meter_number": "<your meter number>",
+    "access_license": "<your access license>",
+}
+
+EndiciaConfig = {
+    "partner_id": "<your partner ID>",
+    "account_id": "<your account ID>",
+    "passphrase": "<your passphrase>"
+}
+
+# Test config dictionaries.
+UPSTestConfig = {
+    "username": "<your username>",
+    "password": "<your password>",
+    "access_license": "<your access license>",
+    "shipper_number": "<your shipper number>"
+}
+
+FedexTestConfig = {
+    "key": "<your API key>",
+    "password": "<your password>",
+    "account_number": "<your account number>",
+    "meter_number": "<your meter number>",
+    "access_license": "<your access license>",
+}
+
+EndiciaTestConfig = {
+    "partner_id": "<any value>",
+    "account_id": "000000",
+    "passphrase": "<any value>"
+}
